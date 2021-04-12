601c19ad614727c25683713fd13ea07a6f6fc6a3
Hoalq-0026 <hoalqbv@gmail.com>
2021-04-12 16:00:06 +0700

    fix comments

--- app/src/test/java/com/quanghoa/apps/lequanghoaunittestexam/ui/ExamViewModelTest.kt
+++ app/src/test/java/com/quanghoa/apps/lequanghoaunittestexam/ui/ExamViewModelTest.kt
@@ -2,6 +2,7 @@ package com.quanghoa.apps.lequanghoaunittestexam.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.quanghoa.apps.lequanghoaunittestexam.model.DiscountPercent
+import com.quanghoa.apps.lequanghoaunittestexam.model.PaymentAmount
 import com.quanghoa.apps.lequanghoaunittestexam.model.UserClassType
 import org.junit.*
 import org.junit.rules.TestRule
@@ -51,6 +52,18 @@ class ExamViewModelTest {
     }
 
     @Test
+    fun calculateDiscount_withUserIsBlackClassType_andTolTalPaymentEqual3K() {
+        val payment = 3000.0
+        val expectDiscount = 150.0
+        viewModel.setTotalPayment(payment)
+        viewModel.updateUserClassType(UserClassType.BLACK.name)
+
+        val discount = viewModel.discountCalculation()
+
+        Assert.assertEquals(expectDiscount, discount, 0.0)
+    }
+
+    @Test
     fun calculateDiscount_withUserIsBlackClassType_andTolTalPaymentFrom3K() {
         val payment = 3001.0
         val expectDiscount = payment * DiscountPercent.BLACK_CLASS_MIN_3K_DISCOUNT_PERCENT
@@ -61,9 +74,29 @@ class ExamViewModelTest {
     }
 
     @Test
+    fun calculateDiscount_withUserIsBlackClassType_andTolTalPaymentEqual5K() {
+        val payment = 5000.0
+        val expectDiscount = 350.0
+        viewModel.setTotalPayment(payment)
+        viewModel.updateUserClassType(UserClassType.BLACK.name)
+        val discount = viewModel.discountCalculation()
+        Assert.assertEquals(expectDiscount, discount, 0.0001)
+    }
+
+    @Test
     fun calculateDiscount_withUserIsBlackClassType_andTolTalPaymentFrom5K() {
         val payment = 5001.0
         val expectDiscount = payment * DiscountPercent.BLACK_CLASS_MIN_5K_DISCOUNT_PERCENT
+        viewModel.setTotalPayment(payment)
+        viewModel.updateUserClassType(UserClassType.BLACK.name)
+        val discount = viewModel.discountCalculation()
+        Assert.assertEquals(expectDiscount, discount, 0.0)
+    }
+
+    @Test
+    fun calculateDiscount_withUserIsBlackClassType_andTolTalPaymentEqual10K() {
+        val payment = 10000.0
+        val expectDiscount = 1500.0
         viewModel.setTotalPayment(payment)
         viewModel.updateUserClassType(UserClassType.BLACK.name)
         val discount = viewModel.discountCalculation()
@@ -92,6 +125,16 @@ class ExamViewModelTest {
     }
 
     @Test
+    fun calculateDiscount_withUserIsGoldClassType_andTolTalPaymentEqual3K() {
+        val payment = 3000.0
+        val expectDiscount = 90.0
+        viewModel.setTotalPayment(payment)
+        viewModel.updateUserClassType(UserClassType.GOLD.name)
+        val discount = viewModel.discountCalculation()
+        Assert.assertEquals(expectDiscount, discount, 0.0)
+    }
+
+    @Test
     fun calculateDiscount_withUserIsGoldClassType_andTolTalPaymentFrom3K() {
         val payment = 3001.0
         val expectDiscount = payment * DiscountPercent.GOLD_CLASS_MIN_3K_DISCOUNT_PERCENT
@@ -102,9 +145,29 @@ class ExamViewModelTest {
     }
 
     @Test
+    fun calculateDiscount_withUserIsGoldClassType_andTolTalPaymentEqual5K() {
+        val payment = 5000.0
+        val expectDiscount = 250.0
+        viewModel.setTotalPayment(payment)
+        viewModel.updateUserClassType(UserClassType.GOLD.name)
+        val discount = viewModel.discountCalculation()
+        Assert.assertEquals(expectDiscount, discount, 0.0)
+    }
+
+    @Test
     fun calculateDiscount_withUserIsGoldClassType_andTolTalPaymentFrom5K() {
         val payment = 5005.0
         val expectDiscount = payment * DiscountPercent.GOLD_CLASS_MIN_5K_DISCOUNT_PERCENT
+        viewModel.setTotalPayment(payment)
+        viewModel.updateUserClassType(UserClassType.GOLD.name)
+        val discount = viewModel.discountCalculation()
+        Assert.assertEquals(expectDiscount, discount, 0.0)
+    }
+
+    @Test
+    fun calculateDiscount_withUserIsGoldClassType_andTolTalPaymentEqual10K() {
+        val payment = 10000.0
+        val expectDiscount = 1000.0
         viewModel.setTotalPayment(payment)
         viewModel.updateUserClassType(UserClassType.GOLD.name)
         val discount = viewModel.discountCalculation()
@@ -132,6 +195,16 @@ class ExamViewModelTest {
     }
 
     @Test
+    fun calculateDiscount_withUserIsSilverClassType_andTolTalPaymentEqual3K() {
+        val payment = 3000.0
+        val expectDiscount = 30.0
+        viewModel.setTotalPayment(payment)
+        viewModel.updateUserClassType(UserClassType.SILVER.name)
+        val discount = viewModel.discountCalculation()
+        Assert.assertEquals(expectDiscount, discount, 0.0)
+    }
+
+    @Test
     fun calculateDiscount_withUserIsSilverClassType_andTolTalPaymentFrom3K() {
         val payment = 3001.0
         val expectDiscount = payment * DiscountPercent.SILVER_CLASS_MIN_3K_DISCOUNT_PERCENT
@@ -142,9 +215,30 @@ class ExamViewModelTest {
     }
 
     @Test
+    fun calculateDiscount_withUserIsSilverClassType_andTolTalPaymentEqual5K() {
+        val payment = 5000.0
+        val expectDiscount = 100.0
+        viewModel.setTotalPayment(payment)
+        viewModel.updateUserClassType(UserClassType.SILVER.name)
+        val discount = viewModel.discountCalculation()
+        Assert.assertEquals(expectDiscount, discount, 0.0)
+    }
+
+
+    @Test
     fun calculateDiscount_withUserIsSilverClassType_andTolTalPaymentFrom5K() {
         val payment = 5001.0
         val expectDiscount = payment * DiscountPercent.SILVER_CLASS_MIN_5K_DISCOUNT_PERCENT
+        viewModel.setTotalPayment(payment)
+        viewModel.updateUserClassType(UserClassType.SILVER.name)
+        val discount = viewModel.discountCalculation()
+        Assert.assertEquals(expectDiscount, discount, 0.0)
+    }
+
+    @Test
+    fun calculateDiscount_withUserIsSilverClassType_andTolTalPaymentEqual10K() {
+        val payment = 10000.0
+        val expectDiscount = 400.0
         viewModel.setTotalPayment(payment)
         viewModel.updateUserClassType(UserClassType.SILVER.name)
         val discount = viewModel.discountCalculation()
@@ -159,6 +253,39 @@ class ExamViewModelTest {
         viewModel.updateUserClassType(UserClassType.SILVER.name)
         val discount = viewModel.discountCalculation()
         Assert.assertEquals(expectDiscount, discount, 0.0)
+    }
+
+    @Test
+    fun giftAccepted_withTotalPayment_is3000k_return_false(){
+        Assert.assertFalse(viewModel.giftAccepted(PaymentAmount.PAYMENT_3K))
+    }
+
+    @Test
+    fun giftAccepted_withTotalPayment_is5000k_return_true(){
+        Assert.assertTrue(viewModel.giftAccepted(PaymentAmount.PAYMENT_5K))
+    }
+
+    @Test
+    fun giftAccepted_withTotalPayment_is10000k_return_true(){
+        Assert.assertTrue(viewModel.giftAccepted(PaymentAmount.PAYMENT_10K))
+    }
+
+    @Test
+    fun updateUserClassType_isBlackClassType_equalUserClassType_isBlack(){
+        viewModel.updateUserClassType(UserClassType.BLACK.name)
+        Assert.assertEquals(UserClassType.BLACK, viewModel.user.value?.classType)
+    }
+
+    @Test
+    fun updateUserClassType_isGoldClassType_equalUserClassType_isGold(){
+        viewModel.updateUserClassType(UserClassType.GOLD.name)
+        Assert.assertEquals(UserClassType.GOLD, viewModel.user.value?.classType)
+    }
+
+    @Test
+    fun updateUserClassType_isSilverClassType_equalUserClassType_isSilver(){
+        viewModel.updateUserClassType(UserClassType.SILVER.name)
+        Assert.assertEquals(UserClassType.SILVER, viewModel.user.value?.classType)
     }
 
 }
